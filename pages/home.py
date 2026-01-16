eddc362d9c12475b79520b90afb3d9724286c049
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-15 23:20:00 -0300

    add home page

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,8 @@
-# Exemplo dentro do pages/home.py
 import streamlit as st
 
 if st.session_state["lang"] == "pt":
     st.title("Bem-vindo")
-    st.write("Esta é a página inicial do ReliaBridge.")
+    st.write("Esta é a página inicial do RainData.")
 else:
     st.title("Welcome")
-    st.write("This is the home page of ReliaBridge.")
+    st.write("This is the home page of RainData.")
