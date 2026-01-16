56041206cb32a9f8cc4f83454e15ca1b3325aaac
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-15 23:23:20 -0300

    init main streamlit file

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 import streamlit as st
 
-# Configuração da página (Sempre a primeira linha)
-st.set_page_config(page_title="ReliaBridge", layout="wide")
+st.set_page_config(page_title="Rain Data", layout="wide")
 
-# Inicializa o estado do idioma se não existir
 if "lang" not in st.session_state:
     st.session_state["lang"] = "pt"
 
-# Cria o seletor na barra lateral (Aparecerá em todas as páginas)
-idioma_selecionado = st.sidebar.selectbox("Language / Idioma", ["Português", "English"], index=0 if st.session_state["lang"] == "pt" else 1)
+idioma_selecionado = st.sidebar.selectbox(
+    "Language / Idioma", ["Português", "English"], index=0 if st.session_state["lang"] == "pt" else 1)
 if idioma_selecionado == "Português":
     st.session_state["lang"] = "pt"
 else:
     st.session_state["lang"] = "en"
 lang = st.session_state["lang"]
 
-# Dicionário com os Títulos do Menu
 titulos_menu = {
-                    "pt": {
-                            "home": "Início",
-                            "longarina": "Projeto Longarina"
-                          },
-                    "en": {
-                            "home": "Home",
-                            "longarina": "Longarina Project"
-                          }
-                }
+    "pt": {
+        "home": "Início",
+        "raindata": "Explorador de Chuva"
+    },
+    "en": {
+        "home": "Home",
+        "raindata": "Rain Explorer"
+    }
+}
 
-# Definição das páginas usando os títulos dinâmicos
-home_page = st.Page("pages/home.py", title=titulos_menu[lang]["home"], icon="🏠", default=True)
-longarina_page = st.Page("pages/longarina_projeto.py", title=titulos_menu[lang]["longarina"], icon="🏗️")
+home_page = st.Page(
+    "pages/home.py", title=titulos_menu[lang]["home"], icon="🏠", default=True)
+raindata_page = st.Page("pages/raindata.py",
+                        title=titulos_menu[lang]["raindata"], icon="🌧️")
 
-# Executa a navegação
-pg = st.navigation([home_page, longarina_page])
+pg = st.navigation([home_page, raindata_page])
 pg.run()
