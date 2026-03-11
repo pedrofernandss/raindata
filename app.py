6a8327875bca1d51991f04f8c24de3d8acfe1057
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-03-10 22:22:59 -0300

    💄 feat: update pages names

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ home_title = ("Início" if st.session_state["lang"] == "pt" else "Home")
 dataset_explorer_title = (
     "Explorador" if st.session_state["lang"] == "pt" else "Dataset Explorer")
 data_analysis_title = (
-    "Cálculo do Ano Hidrológico" if st.session_state["lang"] == "pt" else "Hydrological Year Calculation")
+    "Análise de Dados Hidrológicos" if st.session_state["lang"] == "pt" else "Data Analysis")
 
 home_page = st.Page("pages/home.py", title=home_title, icon="🏠", default=True)
 dataset_explorer_page = st.Page(
     "pages/explorer_page.py", title=dataset_explorer_title, icon="🌧️")
 data_analysis_page = st.Page(
-    "pages/data_analysis_page.py", title=data_analysis_title)
+    "pages/data_analysis_page.py", title=data_analysis_title, icon="📊")
 
 pg = st.navigation([home_page, dataset_explorer_page, data_analysis_page])
 pg.run()
