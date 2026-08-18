098f7a51ed9a20263ac56663fbf87636bef78a85
Prof. Antover Panazzolo Sarmento <146863039+antoverps@users.noreply.github.com>
2026-08-18 15:26:30 -0300

    Update statistic.py

--- src/functions/statistic.py
+++ src/functions/statistic.py
@@ -17,7 +17,7 @@ def compute_cdf(x: list) -> tuple[list, list]:
     return list(x_sorted), list(x_cdf)
 
 
-def verify_probability_distribuition(dataset: pd.DataFrame) -> pd.DataFrame:
+def verify_probability_distribuition(dataset: pd.DataFrame):
     """
     Fit candidate probability distributions to the annual maximum
     precipitation series and compare them using the Kolmogorov-Smirnov
@@ -25,12 +25,22 @@ def verify_probability_distribuition(dataset: pd.DataFrame) -> pd.DataFrame:
 
     The distribution with the smallest KS statistic is selected.
 
-    :param dataset: Annual maximum daily precipitation series
-    :return:
-        [0] DataFrame with fitted distributions and KS statistics,
-        sorted from smallest to largest KS statistic
-        [1] Parameters of the selected distribution
-        [2] SciPy name of the selected distribution
+    Parameters
+    ----------
+    dataset : pd.DataFrame
+        Annual maximum daily precipitation series.
+
+    Returns
+    -------
+    results_df : pd.DataFrame
+        Candidate distributions and corresponding KS statistics,
+        sorted from smallest to largest KS statistic.
+
+    params : tuple
+        Parameters of the selected distribution.
+
+    selected_dist : str
+        SciPy name of the selected distribution.
     """
 
     distribution_list = [
@@ -62,31 +72,25 @@ def verify_probability_distribuition(dataset: pd.DataFrame) -> pd.DataFrame:
     for dist in distribution_list:
 
         if dist == 'genextreme':
-
             dist_name = 'Generalized Extreme Value (GEV)'
             dist_obj = sc.stats.genextreme
             params = dist_obj.fit(x)
 
         elif dist == 'gumbel_r':
-
             dist_name = 'Gumbel'
             dist_obj = sc.stats.gumbel_r
             params = dist_obj.fit(x)
 
         elif dist == 'lognorm':
-
             dist_name = 'Log-Normal'
             dist_obj = sc.stats.lognorm
 
-            # Precipitation is strictly positive;
-            # location is fixed at zero.
             params = dist_obj.fit(
                 x,
                 floc=0
             )
 
         elif dist == 'pearson3':
-
             dist_name = 'Pearson Type III'
             dist_obj = sc.stats.pearson3
             params = dist_obj.fit(x)
@@ -94,9 +98,6 @@ def verify_probability_distribuition(dataset: pd.DataFrame) -> pd.DataFrame:
         else:
             continue
 
-        # KS statistic used as a comparative measure of fit.
-        # The conventional KS p-value is not used for model selection
-        # because distribution parameters are estimated from the same sample.
         ks_stat = sc.stats.kstest(
             x,
             dist,
@@ -121,8 +122,6 @@ def verify_probability_distribuition(dataset: pd.DataFrame) -> pd.DataFrame:
 
     results_df = pd.DataFrame(results)
 
-    # Smaller KS statistic indicates closer agreement
-    # between empirical and fitted CDFs.
     results_df = results_df.sort_values(
         by="Estatística KS",
         ascending=True
